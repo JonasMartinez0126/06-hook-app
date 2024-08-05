f567dfeb9dafd1fa1e0c1b903849d8912aaa00f7
JonasMartinez0126 <127220073+JonasMartinez0126@users.noreply.github.com>
2024-08-05 21:13:19 +0000

    se agregaron mas pruebas

--- test/03-examples/MultipleCustomHooks.test.jsx
+++ test/03-examples/MultipleCustomHooks.test.jsx
@@ -1,10 +1,23 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { MultipleCustomHooks } from "../../src/03-examples/MultipleCustomHooks";
 import { useFetch } from "../../src/hooks/useFetch";
+import { useCounter } from "../../src/hooks/useCounter";
 
 jest.mock('../../src/hooks/useFetch');
+jest.mock('../../src/hooks/useCounter');
 
 describe('Pruebas en <MultipleCustomHooks />', () => {
+    const mockIncrement = jest.fn();
+
+    useCounter.mockReturnValue({
+        counter: 1,
+        increment: mockIncrement
+    });
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
     test('debe de mostrar el componente por defecto', () => { 
         useFetch.mockReturnValue({
             data: null,
@@ -23,7 +36,7 @@ describe('Pruebas en <MultipleCustomHooks />', () => {
 
     });
 
-    test('debe de mostrar un Quote', () => { 
+    test('debe de mostrar un Pokemon', () => { 
         useFetch.mockReturnValue({
             data: {
                 name: "bulbasaur",
@@ -43,5 +56,34 @@ describe('Pruebas en <MultipleCustomHooks />', () => {
         expect( screen.getByText('bulbasaur') );
 
         screen.debug();
+    });
+
+    test('debe de llamar la funcion incrementar', () => {
+        
+        useFetch.mockReturnValue({
+            data: {
+                name: "bulbasaur",
+                id: 1,
+                sprites: {
+                    back_default: "back_default",
+                    back_shiny: "back_shiny",
+                    front_default: "front_default",
+                    front_shiny: "front_shiny",
+                }
+            },
+            isLoading: false,
+            hasError: null
+        });
+
+       
+
+        
+        render(<MultipleCustomHooks />);
+
+        const nextButton = screen.getByRole("button", { name: "Next Pokemon" });
+        fireEvent.click(nextButton);
+ 
+        expect(mockIncrement).toHaveBeenCalled();
+
     });
 });
